5ccc9d241d8f366ec86be2c5883a9d69e533c176
Alejandro Guillen <alejandro.guillen@synergy-gb.com>
2016-02-22 01:05:48 -0430

    case elems == []

--- routes/mongoose_findGraph.js
+++ routes/mongoose_findGraph.js
@@ -8,10 +8,18 @@ router.get('/', function(req, res) {
 
     var app_name = req.query.app;
 
+    console.log(app_name)
+
     Project.find({ _id: app_name }, function(err, elems){
-        console.log(elems);
-        console.log(elems[0].graph);
-        res.send(elems[0].graph);
+        console.log(elems.length)
+        if(typeof(elems)!="undefined" || elems.length > 0 ){
+            console.log(elems);
+            console.log(elems[0].graph);
+            res.send(elems[0].graph);
+        } else{
+            res.status(404).send("project not found");
+        };
+        
     });
 
 });
