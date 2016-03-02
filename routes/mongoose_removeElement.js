da150d5bdeb6704781c6d56b129ec7ec5546842e
Alejandro Guillen <alejandro.guillen@synergy-gb.com>
2016-03-02 00:41:09 -0430

    delete project

--- routes/mongoose_removeElement.js
+++ routes/mongoose_removeElement.js
@@ -3,51 +3,37 @@ var express = require('express');
 var mongoose = require('mongoose');
 var _ = require('lodash');
 var Project = require('./schemas/projectSchema');
+var User = require('./schemas/userSchema');
 var router = express.Router();
 
 router.delete('/', function(req, res) {
 
     var app_name = req.query.app;
-    var elementId = req.query.id;
-
+    var username = req.query.username;
     Project.find({ _id: app_name }, function(err, elems){
-        console.log(app_name);
-        console.log(elementId);
-        console.log(elems);
         if(elems.length != 0){
-            _.remove(elems[0].models['data'], {elementId: elementId});
-            _.remove(elems[0].models['params'], {elementId: elementId});
-            _.remove(elems[0].models['screen'], {elementId: elementId});
-            _.remove(elems[0].models['event'], function(elem){
-                return elem.elementId.split("-")[0] == elementId || elem.elementId.split("-")[1] == elementId;
-            });
-            var graph = JSON.parse(elems[0].graph);
-            _.remove(graph.nodes, function(elem){
-                return elem.id == elementId;
-            });
-            _.remove(graph.edges, function(elem){
-                return elem.source == elementId || elem.target == elementId;
-            });
-            
-            var updateProject = new Project({ 
-                                                _id: elems[0]._id, 
-                                                backgroundImage: elems[0].backgroundImage,
-                                                appLogo: elems[0].appLogo,
-                                                models:{data: elems[0].models['data'],
-                                                        screen: elems[0].models['screen'],
-                                                        params: elems[0].models['params'],
-                                                        event: elems[0].models['event']},
-                                                graph: JSON.stringify(graph)
-                                            });
-
             elems[0].remove(function(){
-                console.log('remove')
-            });
-
-            updateProject.save(function(err, elems){
-                console.log('model updated');
-            });
-             
+                User.find({_id: username}, function(err, elems){
+                    if(elems.length != 0){
+                        var projects = _.filter(elems[0].projects, function(e){
+                                                                                return e != app_name
+                                                                            });
+                        var updateUser = new User({
+                                                    _id: elems[0]._id,
+                                                    password: elems[0].password,
+                                                    projects: projects
+                                                });
+                        elems[0].remove(function(){
+                            updateUser.save(function(){
+                                res.send("username updated");
+                            })    
+                        });
+                        
+                    } else {
+                        res.status(404).send("not found");
+                    }
+                })
+            });             
         }else{
             res.status(404).send("not found");
         };
